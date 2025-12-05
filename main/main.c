f209be9d68ec15ac3e1afb3abf22ddcf0b641ff3
tharu62 <tharinduxdeshan@gmail.com>
2025-12-05 18:30:27 +0100

    structuring the main.c file

--- main/main.c
+++ main/main.c
@@ -1,52 +1,101 @@
 /*
  * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
  * SPDX-License-Identifier: CC0-1.0
+ * 
+ * This software is a firmaware for an esp32 board running on a 4 motor drone.
+ * It is responsible of stabilization and speed control of the drone 
+ * and also receiving and executing the commands from the user.   
+ * The use of this software is allowed by anyone in any capacity for free. 
+ * 		
+ *					The author, 
+ * 					Deshan Tharindu Edirisinghe Edirisinghe Mudiyanselage.
+ *
  */
 
+#include <stdbool.h>
 #include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+/*#include <inttypes.h>
+#include "sdkconfig.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
-#include "esp_system.h"
+#include "esp_system.h"*/
+
+
+typedef enum {
+	
+	UP = 0,
+	DOWN = 1,
+	RIGHT = 2,
+	LEFT = 3,
+	FORWARD = 4,
+	BACK = 5
+	
+}cmd;
+
+typedef struct {
+	
+	float pwm1;
+	float pwm2;
+	float pwm3;
+	float pwm4;
+	cmd user_input;
+	
+	
+} state;
+
+
+bool input_from_user();
+
+
 
 void app_main(void)
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
+    printf("ESP32 BOARD STARTED!");
     fflush(stdout);
-    esp_restart();
+	
+	/** init tools :
+		- init motors pwm
+		- init mpu6050 sensor com
+		- init user input com
+	*/ 
+	
+	
+	// main loop for drone controll and input read
+	while(true){
+			
+		// 1. check input from user
+		if(input_from_user()){
+			
+		}
+		
+		// 2. if input is present -> set new state
+		else {
+			
+		}
+		
+		// 3. else keep last state
+		
+		// 4. run pid controll using current state, sensor mesurement and last state.
+		
+		// 5. go back to (1)
+	
+		;;
+	}
+	
+	
+	
+	return;
 }
+
+
+bool input_from_user(){
+	
+	
+	
+	return false;
+}
+
+
